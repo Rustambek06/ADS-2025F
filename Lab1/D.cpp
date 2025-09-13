13dab6331dd88503799339ef51e31b55c967fbb4
Rustambek <rusik.khanov06@gmail.com>
2025-09-13 13:20:23 +0500

    Lab1-D

--- Lab1/D.cpp
+++ Lab1/D.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
+#include <stack>
 #include <string>
-
-bool is_balanced(const std::string& s, int l, int r) {
-    if (l > r) return true;  // пустая строка
-
-    if (s[l] == s[r]) {
-        return is_balanced(s, l + 1, r - 1);
-    }
-
-    for (int mid = l; mid < r; ++mid) {
-        if (is_balanced(s, l, mid) && is_balanced(s, mid + 1, r)) {
-            return true;
-        }
-    }
-
-    return false;
-}
 
 int main() {
     std::string s;
     std::cin >> s;
 
-    if (is_balanced(s, 0, static_cast<int>(s.size()) - 1)) {
+    std::stack<char> st;
+    for (char c : s) {
+        if (!st.empty() && st.top() == c) {
+            st.pop();
+        } else {
+            st.push(c);
+        }
+    }
+
+    if (st.empty()) {
         std::cout << "YES\n";
     } else {
         std::cout << "NO\n";
