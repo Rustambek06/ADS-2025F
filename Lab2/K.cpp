7bfd44288493857e72956924f4c8bdce692c7729
Rustambek <rusik.khanov06@gmail.com>
2025-10-15 13:52:11 +0500

    Lab3-C

---
+++ Lab2/K.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <>
+
+int main() {
+    int T; 
+    std::cin >> T;
+    while (T--) {
+        int N; 
+        std::cin >> N;
+
+        std::string s;
+        std::getline(std::cin, s);
+
+        for (char ch: s) {
+            if (ch != ' ') {
+
+            }
+        }
+    }
+} 
